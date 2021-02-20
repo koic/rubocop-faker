548881ddbe1b82b3d69c6295c89ab8c78c9dea10
Koichi ITO <koic.ito@gmail.com>
2021-02-21 05:09:33 +0900

    Corrects RuboCop offense
    This commit corrects the following RuboCop offense.
    
    ```console
    % bundle exec rubocop -a
    Inspecting 13 files
    ..........C..
    
    Offenses:
    
    spec/rubocop/cop/faker/deprecated_arguments_spec.rb:4:1: C: [Corrected]
    Layout/EmptyLinesAroundBlockBody: Extra empty line detected at block
    body beginning.
    spec/rubocop/cop/faker/deprecated_arguments_spec.rb:4:3: C: [Corrected]
    InternalAffairs/RedundantDescribedClassAsSubject: Remove the redundant
    subject.
    subject(:cop) { described_class.new(config) }
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    13 files inspected, 2 offenses detected, 2 offenses corrected
    ```

--- spec/rubocop/cop/faker/deprecated_arguments_spec.rb
+++ spec/rubocop/cop/faker/deprecated_arguments_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Faker::DeprecatedArguments, :config do
-  subject(:cop) { described_class.new(config) }
-
   # RuboCop::ConfigLoader.default_configuration['Faker/DeprecatedArguments']
   let(:cop_config) do
     {
